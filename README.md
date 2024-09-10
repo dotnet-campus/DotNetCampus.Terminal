00b061e7ff690a467c076d0c3e56a056532980b6
walterlv <xblero@gmail.com>
2024-09-10 16:26:43 +0800

    First project files.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Walterlv Terminal
+
+名字还没取。等做好了能用了再取名字吧！
---
+++ src/Walterlv.Terminal/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Walterlv.Terminal;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}

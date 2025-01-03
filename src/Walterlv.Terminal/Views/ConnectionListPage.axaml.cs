d888e7bc536b62f835fd3f58c2121bc165094cd5
walterlv <xblero@gmail.com>
2025-01-03 11:26:49 +0800

    修改样式

---
+++ src/Walterlv.Terminal/Views/ConnectionListPage.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Walterlv.Terminal.Views;
+
+public partial class ConnectionListPage : UserControl
+{
+    public ConnectionListPage()
+    {
+        InitializeComponent();
+    }
+}
+
